644d1715a123ca479bc1766d937e6070477ab099
ochi7715 <92065046+ochi7715@users.noreply.github.com>
2023-08-25 19:02:20 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -32,3 +32,23 @@ document.addEventListener("DOMContentLoaded", function() {
     securityNumberDisplay.textContent = securityNumber;
   }
 });
+
+
+var acc = document.getElementsByClassName("FAQ-accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
